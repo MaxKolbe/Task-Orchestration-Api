c4043ddc3ae14ff98e6804ff773db153175eea90
Maxmillian <maximilianogbuabor@gmail.com>
2026-01-17 21:16:32 +0100

    refact: negligible refactors

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -7,34 +7,29 @@ export class Todoservice {
 
   async getTodo(limit: number, skip: number) {
     const result = await this.newdb.select().from(todos).orderBy(asc(todos.task)).limit(limit).offset(skip);
-
     return result;
   }
 
   async getOneTodo(id: string) {
     const result = await this.newdb.select().from(todos).where(eq(todos.id, id));
-
     return result[0];
-  }
+  }  
 
   async postTodo(task: string, isDone: boolean) {
     const result = await this.newdb
       .insert(todos)
       .values({ id: sql`uuid_generate_v4()`, task, isdone: isDone })
       .returning();
-
     return result[0];
   }
 
   async updateTodo(id: string, task: string, isDone: boolean) {
     const result = await this.newdb.update(todos).set({ task, isdone: isDone }).where(eq(todos.id, id)).returning();
-
     return result[0];
   }
 
   async deleteTodo(id: string) {
     const result = await this.newdb.delete(todos).where(eq(todos.id, id)).returning();
-
     return result[0];
   }
 }
